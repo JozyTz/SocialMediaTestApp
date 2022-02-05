7a5f99cd4cf7413b1af8d51ed1982a796a5d077c
Westin <westinc@iastate.edu>
2022-02-04 21:24:25 -0600

    Did not work

--- experiments/Westin/Testing2/src/main/java/com/example/demo/Testing2Controller.java
+++ experiments/Westin/Testing2/src/main/java/com/example/demo/Testing2Controller.java
@@ -1,5 +1,20 @@
 package com.example.demo;
 
-public class Testing2Controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RestController
+class Testing2Controller {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello and welcome to COMS 309: " + name;
+    }
 }
+
