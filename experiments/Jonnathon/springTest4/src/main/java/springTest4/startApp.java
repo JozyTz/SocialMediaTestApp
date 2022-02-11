9b0442d6d59960368c21ffe45c97d3dcf0a63d7d
jonb <jonb@iastate.edu>
2022-02-11 10:24:01 -0600

    Demo 1

---
+++ experiments/Jonnathon/springTest4/src/main/java/springTest4/startApp.java
@@ -0,0 +1,12 @@
+package springTest4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class startApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(startApp.class, args);
+    }
+}
