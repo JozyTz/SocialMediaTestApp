080924514561ee93c15186c5199f634c5b8a66ee
jonb <jonb@iastate.edu>
2022-02-18 10:56:28 -0600

    beginning user creation and talking to db

---
+++ backend/app/src/main/java/project/app/model/User.java
@@ -0,0 +1,59 @@
+package project.app.model;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String username;
+	private String password;
+	
+
+	public User() {}
+	
+	public User(String username, int id) {
+		this.username = username;
+		this.id = id;
+	}
+	
+	
+	public Integer getID() {
+		return id;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setID(String username) {
+		this.username = username;
+	}
+	
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+    @Override
+    public String toString() {
+        return "UserName: " + username + "\nID: " + id ;
+    }
+
+	
+}
---
+++ backend/app/src/main/java/project/app/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package project.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import project.app.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+
+}
