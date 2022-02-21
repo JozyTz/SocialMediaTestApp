7739139d4d3f26177bae8c9ec17600953b5611b2
Thomas Glass <trglass@iastate.edu>
2022-02-21 12:30:50 -0600

    New login page

---
+++ experiments/Thomas/oldLogin/app/src/main/java/com/example/notesapp/SecondPage.java
@@ -0,0 +1,35 @@
+package com.example.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class SecondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+
+        TextView loggedin = (TextView) findViewById(R.id.loggedin);
+        EditText inputText = (EditText) findViewById(R.id.inputText);
+
+        MaterialButton namebutton = (MaterialButton) findViewById(R.id.namebutton);
+
+        namebutton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                loggedin.setText("Hi " + inputText.getText());
+                System.out.println("Button clicked");
+            }
+
+            });
+        }
+}
