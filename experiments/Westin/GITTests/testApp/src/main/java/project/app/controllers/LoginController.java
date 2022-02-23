9c442de86183a9eb507a7d7d406da11d0c74631e
Westin <westinc@iastate.edu>
2022-02-22 23:00:22 -0600

    Added TestApp to experiments folder

---
+++ experiments/Westin/GITTests/testApp/src/main/java/project/app/controllers/LoginController.java
@@ -0,0 +1,45 @@
+package project.app.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import project.app.model.User;
+import project.app.repository.UserRepository;
+
+@RestController
+public class LoginController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	
+	@GetMapping("users/all")
+	List<User> GetAllUsers(){
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("users/{id}")
+	List<User> GetUserByID(){
+		return userRepository.findAll();
+		//TODO
+	}
+	
+	@PostMapping("/users/create")
+	User newUser(@RequestBody User newUser) {
+		return userRepository.save(newUser);
+	}
+	
+	@DeleteMapping("/users/{id}")
+    String deleteUser(@PathVariable int id){
+        userRepository.deleteById(id);
+        return "Success";
+	}
+
+}
