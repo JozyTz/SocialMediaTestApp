0a3b390b0cd7c5dca8dedb1c3992d04792ed52ef
jonb <jonb@iastate.edu>
2022-02-27 18:20:19 -0600

    comments/organizing

--- backend/app/src/main/java/project/app/repository/UserRepository.java
+++ backend/app/src/main/java/project/app/repository/UserRepository.java
@@ -4,9 +4,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import project.app.model.User;
 
+/**
+ * Repo of users
+ * 
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	/**
+	 * finds a user using their ID
+	 * @param id
+	 * @return
+	 */
 	User findById(int id);
+	
+	/**
+	 * deletes a user by their ID
+	 * @param id
+	 */
     void deleteById(int id);
 
 
