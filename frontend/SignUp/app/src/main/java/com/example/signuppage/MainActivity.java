581386e95342def671f5a319b92def1bd7a2ed75
Thomas Glass <trglass@iastate.edu>
2022-03-01 16:05:35 -0600

    Updating sign up page

--- frontend/SignUp/app/src/main/java/com/example/signuppage/MainActivity.java
+++ frontend/SignUp/app/src/main/java/com/example/signuppage/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.signuppage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button singUp;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        singUp = (Button)findViewById(R.id.button);
+
+        singUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                signUpAttempt();
+            }
+        });
+    }
+
+    public void signUpAttempt()
+    {
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText email = (EditText)findViewById(R.id.email);
+        EditText password = (EditText)findViewById(R.id.password);
+        EditText confirmed = (EditText)findViewById(R.id.confirmedPassword);
+        if(confirmed == password)
+        {
+            Toast.makeText(MainActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            Toast.makeText(MainActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+        }
     }
 }
