d7dd934dfb2c71b46a4537962afe482f25b5a9ef
Thomas Glass <trglass@iastate.edu>
2022-03-02 20:47:36 -0600

    Ensuring password length and attempting volley to backend

--- frontend/SignUp/app/src/main/java/com/example/signuppage/MainActivity.java
+++ frontend/SignUp/app/src/main/java/com/example/signuppage/MainActivity.java
@@ -3,14 +3,27 @@ package com.example.signuppage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity
 {
     Button singUp;
+    private com.android.volley.toolbox.Volley Volley;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,20 +37,48 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                signUpAttempt();
+                try {
+                    signUpAttempt();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    public void signUpAttempt()
-    {
+    public void signUpAttempt() throws JSONException {
         EditText username = (EditText)findViewById(R.id.username);
         EditText email = (EditText)findViewById(R.id.email);
         EditText password = (EditText)findViewById(R.id.password);
         EditText confirmed = (EditText)findViewById(R.id.confirmedPassword);
-        if(confirmed == password)
+        String url = "http://coms-309-025.class.las.iastate.edu:8080/users/create/";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JSONObject object = new JSONObject();
+
+        if(password.getText().toString().equals(confirmed.getText().toString()) && password.getText().toString().length() >= 5)
         {
-            Toast.makeText(MainActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(MainActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+            try {
+                //input your API parameters
+                object.put("username","tglass");
+                object.put("password","glass");
+                object.put("email","tglass@gmail.com");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, object, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Toast.makeText(getApplicationContext(), "Worked", Toast.LENGTH_SHORT).show();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
         }else
         {
             Toast.makeText(MainActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
