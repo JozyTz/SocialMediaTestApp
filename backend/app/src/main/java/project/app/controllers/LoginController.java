c219359ee909355e87e4db7ca29574f059dfe682
jonb <jonb@iastate.edu>
2022-03-03 00:27:24 -0600

    testing new login controller functionality for kaya.
    JSONResponse util class to handle this. UserRepo edit to make this all
    work

--- backend/app/src/main/java/project/app/controllers/LoginController.java
+++ backend/app/src/main/java/project/app/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package project.app.controllers;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import project.app.model.User;
 import project.app.repository.UserRepository;
+import project.app.util.JSONResponse;
 
 @RestController
 public class LoginController {
@@ -38,21 +40,38 @@ public class LoginController {
         return "Success";
 	}
 	
-	@PostMapping(value = "/login", produces = "application/json")
-	User login(@RequestBody User auth) {
-		User user = userRepository.findByUsername(auth.getUsername());
+//	@PostMapping(value = "/login", produces = "application/json")
+//	User login(@RequestBody User auth) {
+//		User user = userRepository.findByUsername(auth.getUsername(), auth.getPassword());
+//		if (user != null) {
+//			String password = user.getPassword();
+//			if (password.equals(auth.getPassword())) {
+//				return user;
+//			}
+//			
+//			else {
+//				System.out.println("Invalid password");
+//				return null;
+//			}
+//		}
+//		System.out.println("Invalid user");
+//		return null;
+//	}
+	
+	/**
+	 * Login with json output for both success and fail
+	 * @param auth
+	 * @return
+	 */
+	@PostMapping(value = "/login")
+	public @ResponseBody  JSONResponse login(@RequestBody Map<String, Object> auth) {
+		User user = userRepository.findByUsernameAndPassword((String) auth.get("username"),
+				(String) auth.get("password"));
+		
 		if (user != null) {
-			String password = user.getPassword();
-			if (password.equals(auth.getPassword())) {
-				return user;
-			}
-			
-			else {
-				System.out.println("Invalid password");
-				return null;
-			}
+			return new JSONResponse(true, user);
 		}
-		System.out.println("Invalid user");
-		return null;
+
+		return new JSONResponse(false, "Invalid user/pass");
 	}
 }
