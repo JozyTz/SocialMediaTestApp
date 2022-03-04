7581aa29c8eba2abd94356c4290cc608013d2367
Thomas Glass <trglass@iastate.edu>
2022-03-04 17:11:50 -0600

    Pushing experimental settings page.

---
+++ experiments/Thomas/Settings/app/src/main/java/com/example/settings/userSettings.java
@@ -0,0 +1,14 @@
+package com.example.settings;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class userSettings extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+}
