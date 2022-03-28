c8c9ebe2c28b10c4366a7a404dc0bd4005966fd3
jonb <jonb@iastate.edu>
2022-03-28 01:29:00 -0500

    included post and user links to comments

--- backend/app/src/main/java/project/app/model/Comment.java
+++ backend/app/src/main/java/project/app/model/Comment.java
@@ -2,6 +2,9 @@ package project.app.model;
 
 import javax.persistence.*;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity(name = "Comment")
@@ -12,12 +15,21 @@ public class Comment {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@JsonIgnore
-	@ManyToOne(cascade = CascadeType.ALL)
-	private User user;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Post post;
 	
 	@Column
-	private String commentString;
+	private String message;
 	
 	@Column
 	private int likes;
@@ -26,13 +38,9 @@ public class Comment {
 	private int dislikes;
 	
 
-	/**
-	 * Comment constructor
-	 * @param username
-	 * @param commentString
-	 */
-	public Comment(User user) {
+	public Comment(Long id, User user, Post post, String message) {
 		this.user = user;
+		this.message = message;
 	}
 	
 	public Comment() {}
@@ -44,9 +52,19 @@ public class Comment {
 	public User getUser() {
 		return user;
 	}
-	
 	public void setUser(User user) {
 		this.user = user;
+	}
+	
+	
+	/**
+	 * post getter/setters
+	 */
+	public Post getPost() {
+		return post;
+	}
+	public void setPost(Post post) {
+		this.post = post;
 	}
 	
 	
@@ -54,13 +72,12 @@ public class Comment {
 	 * Comment getter/setters
 	 */
 	public String getComment() {
-		return commentString;
+		return message;
 	}
-	
-	public void setComment(String commentString) {
-		this.commentString = commentString;
+	public void setComment(String message) {
+		this.message = message;
 	}
-	
+
 	
 	/**
 	 * Like getter/setters
@@ -68,7 +85,6 @@ public class Comment {
 	public int getLikes() {
 		return likes;
 	}
-	
 	public void setLikes(int likes) {
 		this.likes = likes;
 	}
@@ -80,7 +96,6 @@ public class Comment {
 	public int getDislikes() {
 		return dislikes;
 	}
-	
 	public void setDislikes(int dislikes) {
 		this.dislikes = dislikes;
 	}
