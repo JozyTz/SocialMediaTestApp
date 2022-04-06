7447f19608c40bbe3b9459dfa2275b7dfa89c0ff
Westin <westinc@iastate.edu>
2022-04-06 00:30:56 -0500

    Made Post Controller

---
+++ backend/app/src/main/java/project/app/controllers/PostController.java
@@ -0,0 +1,29 @@
+package project.app.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import project.app.model.Comment;
+import project.app.model.Post;
+import project.app.model.User;
+import project.app.repository.CommentRepository;
+import project.app.repository.PostRepository;
+import project.app.repository.UserRepository;
+import project.app.util.JSONResponse;
+
+@RestController
+public class PostController {
+
+	@Autowired
+	PostRepository postRepo;
+	
+	@Autowired
+	CommentRepository commentRepo;
+	
+	@Autowired
+	UserRepository userRepo;
+	
+}
