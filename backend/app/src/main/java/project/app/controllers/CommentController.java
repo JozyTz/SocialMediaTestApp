108b6506fd5047c558cd01344edf2551c954999f
jonb <jonb@iastate.edu>
2022-04-07 01:43:25 -0500

    Update backend/app/src/main/java/project/app/controllers/CommentController.java

--- backend/app/src/main/java/project/app/controllers/CommentController.java
+++ backend/app/src/main/java/project/app/controllers/CommentController.java
@@ -1,4 +1,3 @@
-<<<<<<< backend/app/src/main/java/project/app/controllers/CommentController.java
 package project.app.controllers;
 
 import java.util.List;
@@ -26,63 +25,7 @@ public class CommentController {
 	
 	@Autowired
 	PostRepository postRepo;
-	
-	
-	@PostMapping(path = "/comment/{userId}/{postId}")
-	public @ResponseBody JSONResponse createComment (@RequestBody Comment comment, @PathVariable("userId") Long userIdNum, @PathVariable("postId") Long postIdNum) {
-		
-		User tmpUser = userRepo.findById(userIdNum).get();
-		Post tmpPost = postRepo.findById(postIdNum).get();
-		
-		if (tmpUser != null && tmpPost != null) {
-			//comment.setUser(tmpUser);
-			tmpUser.addComment(comment);
-			comment.setPost(tmpPost);
-			commentRepo.save(comment);
-			return new JSONResponse(true, comment);
-		}
-		else {
-			return new JSONResponse(false, "Invalid user and/or Post");
-		}
-	}
-	
-	
-	@GetMapping(path = "/users/{userId}/comments")
-		public List<Comment> getUserComments (@PathVariable("userId") long userIdNum) {
-			User user = userRepo.findById(userIdNum);
-			return user.getComments();
-	}
-	
-	
-}
-=======
-package project.app.controllers;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import project.app.model.Comment;
-import project.app.model.Post;
-import project.app.model.User;
-import project.app.repository.CommentRepository;
-import project.app.repository.PostRepository;
-import project.app.repository.UserRepository;
-import project.app.util.JSONResponse;
-
-@RestController
-public class CommentController {
-	@Autowired
-	CommentRepository commentRepo;
-	
-	@Autowired
-	UserRepository userRepo;
-	
-	@Autowired
-	PostRepository postRepo;
-	
 	/**
 	 *	Create comment on a post
 	 */
@@ -160,11 +103,14 @@ public class CommentController {
 			return user.getComments();
 	}
 
+
+	/**
+	* Get all comments on a post
+	*/
 	@GetMapping(path = "/posts/{postId}/comments")
 	public List<Comment> getPostComments (@PathVariable("postId") long postIdNum)
 	{
 		Post post = postRepo.findById(postIdNum);
 		return post.getComments();
 	}
->>>>>>> backend/app/src/main/java/project/app/controllers/CommentController.java
 }
