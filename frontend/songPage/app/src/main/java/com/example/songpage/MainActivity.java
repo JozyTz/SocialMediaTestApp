610fdeae224357849721976e3a31cf1feebb5b95
Thomas Glass <trglass@iastate.edu>
2022-04-08 08:53:28 -0500

    updating directory

--- frontend/songPage/app/src/main/java/com/example/songpage/MainActivity.java
+++ frontend/songPage/app/src/main/java/com/example/songpage/MainActivity.java
@@ -12,9 +12,10 @@ import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity
 {
-    Button username;
     TextView songTitle;
     Button picture;
+    Button username;
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,9 +23,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username = (Button)findViewById(R.id.username);
         songTitle = (TextView)findViewById(R.id.songTitle);
+        username = (Button)findViewById(R.id.username);
         picture = (Button)findViewById(R.id.profilePicture);
+        playButton = (Button)findViewById(R.id.playButton);
 
         username.setText("Username");
         songTitle.setText("Song title here.");
@@ -36,10 +38,29 @@ public class MainActivity extends AppCompatActivity
                 linkToProfile();
             }
         });
+        username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                linkToProfile();
+            }
+        });
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                playSong();
+            }
+        });
     }
 
     public void linkToProfile()
     {
         Toast.makeText(MainActivity.this, "Links to the profile page.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void playSong()
+    {
+        Toast.makeText(MainActivity.this, "Plays the song.", Toast.LENGTH_SHORT).show();
     }
 }
