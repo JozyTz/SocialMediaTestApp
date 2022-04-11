2abe9c1f6b4d6f7feecc2daeb42b0c20b4a1d332
Westin <westinc@iastate.edu>
2022-04-11 18:52:02 -0500

    Test

--- backend/app/src/main/java/test.java
+++ backend/app/src/main/java/test.java
@@ -1,13 +1,18 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class test {
 
-//	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 //		// TODO Auto-generated method stub
+//		File file = new File("/home/music/abc.txt");
 //		BufferedReader in = new BufferedReader(new FileReader("\\home\\music\\abc.txt"));
-//		String line = in.readLine();
+//		String line = Files.readAllLines(Paths.get("/home/music/abc.txt")).get(0);
 //		System.out.println(line);
-//	}
+	}
 
 }
