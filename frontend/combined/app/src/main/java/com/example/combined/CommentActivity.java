47c9168621c36b199341d1a0024ef3ddb816915e
kzdan <kzdan@iastate.edu>
2022-05-04 20:49:15 -0500

    resolved import java_websockets error

--- frontend/combined/app/src/main/java/com/example/combined/CommentActivity.java
+++ frontend/combined/app/src/main/java/com/example/combined/CommentActivity.java
@@ -1,6 +1,22 @@
 package com.example.combined;
 
 
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +26,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CommentActivity extends AppCompatActivity {
+
+    private WebSocketClient cc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
