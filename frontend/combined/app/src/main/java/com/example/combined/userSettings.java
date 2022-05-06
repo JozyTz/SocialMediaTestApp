496bedc50ae8820c372cd190af8f4403be8d2cf6
Thomas Glass <tglass2001@gmail.com>
2022-05-05 21:50:59 -0500

    Added options to menu bar and implemented logout

--- frontend/combined/app/src/main/java/com/example/combined/userSettings.java
+++ frontend/combined/app/src/main/java/com/example/combined/userSettings.java
@@ -1,14 +1,36 @@
 package com.example.combined;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class userSettings extends AppCompatActivity
 {
+    Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+
+        logout = (Button) findViewById(R.id.logoutButton);
+
+        logout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                logoutAction();
+            }
+        });
+    }
+
+    public void logoutAction()
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
